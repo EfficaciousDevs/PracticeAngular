6936dc5eef520768e02ba43ecf0eef09d4635ff3
Akshay <aman.akshay88@gmail.com>
2021-06-29 13:21:10 +0530

    Initial Commit: Angular Files.

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Rohit Sharma' },
+  { id: 2, name: 'Shikhar Dhawan' },
+  { id: 3, name: 'Virat Kohli' },
+  { id: 4, name: 'KL Rahul' },
+  { id: 5, name: 'Rishabh Pant' },
+  { id: 6, name: 'Hardik Pandya' },
+  { id: 7, name: 'Ravindra Jadeja' },
+  { id: 8, name: 'Mohd. Shami' },
+  { id: 9, name: 'Bhuvneshwar Kumar' },
+  { id: 10, name: 'R Ashwin' },
+  { id: 11, name: 'Jasprit Bumrah' },
+];
